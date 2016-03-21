ffaec6f7c4091dd37f1f3cdb834ce374c3b29aa4
ILUXONCHIK ILUXA <iluxon4ik@hotmail.com>
2016-03-21 21:01:15 +0000

    lab_06 demo configured + git rm --cached

--- lab_04/ttt-rmi-server/src/main/java/ttt/TTT.java
+++ lab_04/ttt-rmi-server/src/main/java/ttt/TTT.java
@@ -54,7 +54,7 @@ public class TTT extends UnicastRemoteObject implements TTTService {
     @Override
     public int checkWinner() throws RemoteException {
     	  int i;
-    	  /* Check for a winning line - diagonals first */     
+    	  /* Check for a winning line - diagonals first */
     	  if((board[0][0] == board[1][1] && board[0][0] == board[2][2]) ||
     	     (board[0][2] == board[1][1] && board[0][2] == board[2][0])) {
     		  if (board[1][1]=='X')
